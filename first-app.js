2aa455c21d36a97553f323d54f295b35546e08f6
fx15164 <khoannfx15164@funix.edu.vn>
2022-08-11 11:15:43 +0700

    complete Lab1.4

--- first-app.js
+++ first-app.js
@@ -2,6 +2,15 @@ const http = require('http');
 
 const server = http.createServer((req, res) => {
     console.log(req.method, req.headers);
+
+    if(req.url == '/') {
+        res.setHeader('Content-Type', 'text/html');
+        res.write('<html>');
+        res.write('<head><title>Enter Message</title></head>')
+        res.write('<body><form action="/message" method="post"><input type="text"/><button>Send</button></form></body>')
+        res.write('<html>');
+        return res.end();
+    }
     //
     res.setHeader('Content-Type', 'text/html');
     res.write('<html>');
