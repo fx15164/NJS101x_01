42d4cfaf42e18b9d6aa9d83bd6d5843d6f77fa68
fx15164 <khoannfx15164@funix.edu.vn>
2022-08-14 22:18:59 +0700

    add 3 mdw

--- first-app.js
+++ first-app.js
@@ -4,12 +4,16 @@ const express = require('express');
 
 const app = express();
 
-app.use((req, res, next) => {
-    console.log('In the middleware!');
+app.use('/', (req, res, next) => {
+    console.log('THis alway run!');
     next();
 });
 
-app.use((req, res, next) => {
+app.use('/add-product', (req, res, next) => {
+    res.send('<h1>The "Add product" Page</h1>');
+});
+
+app.use('/', (req, res, next) => {
     console.log('In another middleware!');
     res.send('<h1>Hello from express</h1>');
 });
