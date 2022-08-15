c06366f63125811d06c6dfc86260a64d9dfdf69d
fx15164 <khoannfx15164@funix.edu.vn>
2022-08-15 15:08:03 +0700

    lab2.13

--- first-app.js
+++ first-app.js
@@ -7,13 +7,15 @@ const shopRouter = require('./routes/shop');
 
 const app = express();
 
+const rootDir = require('./util/path');
+
 app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use('/admin', adminRouter);
 app.use(shopRouter);
 
 app.use((req, res) => {
-    res.status(404).sendFile(path.join(__dirname, '/views/404.html'));
+    res.status(404).sendFile(path.join(rootDir, '/views/404.html'));
 });
 
 
