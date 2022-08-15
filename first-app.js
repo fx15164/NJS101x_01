a07993842a1f4af8bf8968fa30604d72ea13aa05
fx15164 <khoannfx15164@funix.edu.vn>
2022-08-15 14:11:57 +0700

    add router

--- first-app.js
+++ first-app.js
@@ -1,31 +1,15 @@
-const http = require('http');
 const bodyParser = require('body-parser');
-
 const express = require('express');
+
+const adminRouter = require('./routes/admin');
+const shopRouter = require('./routes/shop');
 
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: false }));
 
-app.use('/', (req, res, next) => {
-    console.log('THis alway run!');
-    next();
-});
+app.use(adminRouter);
+app.use(shopRouter);
 
-app.use('/add-product', (req, res, next) => {
-    res.send('<form action="/product" method="POST"><input type="text" name="title"/><button type="submit">Send</button></form>');
-});
 
-app.post('/product', (req, res) => {
-    console.log(req.body);
-    res.redirect('/');
-})
-
-app.use('/', (req, res, next) => {
-    console.log('In another middleware!');
-    res.send('<h1>Hello from express</h1>');
-});
-
-const server = http.createServer(app);
-
-server.listen(3000);
+app.listen(3000);
