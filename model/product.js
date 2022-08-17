f7663f7a6e719af719cfbe912292047845fac6f4
fx15164 <khoannfx15164@funix.edu.vn>
2022-08-17 16:48:28 +0700

    lab4.6

--- model/product.js
+++ model/product.js
@@ -1,38 +1,39 @@
 const path = require('path');
 const fs = require('fs');
 
+const getProductsPromise = cb => {
+    fs.readFile(
+        path.join(__dirname, '../data/product.json'),
+        'utf-8',
+        (err, content) => {
+            if(err)
+                cb([]);
+            else 
+                cb(JSON.parse(content));
+        }
+    )
+}
+
 module.exports = class Product {
     constructor(title) {
         this.title = title;
     }
 
     save() {
-        const p = path.join(__dirname, '../data/product.json');
-        fs.readFile(p, 'utf-8' ,(err, content) => {
-                let products = [];
-                if(!err) {
-                    products = JSON.parse(content);
-                }
-                products.push(this);
-                fs.writeFile(p, JSON.stringify(products), err => {
+        getProductsPromise(products => {
+            products.push(this);
+            fs.writeFile(
+                path.join(__dirname, '../data/product.json'), 
+                JSON.stringify(products), 
+                err => {
                     if (err) {
                         console.log(err);
-                    }
-                })
-            }
-        )
+                    }}
+                )
+        })
     }
 
     static fetchAll(cb) {
-        fs.readFile(
-            path.join(__dirname, '../data/product.json'),
-            'utf-8',
-            (err, content) => {
-                if(err)
-                    cb([]);
-                else 
-                    cb(JSON.parse(content));
-            }
-        )
+        getProductsPromise(cb);
     }
 }
