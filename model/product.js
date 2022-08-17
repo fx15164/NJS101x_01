758078ea4aac95b1d55394a1b428adc605e0ae53
fx15164 <khoannfx15164@funix.edu.vn>
2022-08-17 14:33:08 +0700

    lab4.4

--- model/product.js
+++ model/product.js
@@ -1,4 +1,5 @@
-const products = [];
+const path = require('path');
+const fs = require('fs');
 
 module.exports = class Product {
     constructor(title) {
@@ -6,10 +7,26 @@ module.exports = class Product {
     }
 
     save() {
-        products.push(this);
+        const p = path.join(__dirname, '../data/product.json');
+        fs.readFile(p, (err, content) => {
+                const products = [];
+                if(!err)
+                    products = JSON.parse(content);
+                products.push(this);
+                fs.writeFile(p, JSON.stringify(products), err => console.log(err))
+            }
+        )
     }
 
     static fetchAll() {
-        return products;
+        fs.readFile(
+            path.join(__dirname, '../data/product.json'),
+            (err, content) => {
+                if(err)
+                    return [];
+                else 
+                    return JSON.parse(content);
+            }
+        )
     }
 }
