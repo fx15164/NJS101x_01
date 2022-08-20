e0a96ac25a01ed9a6ef19da926f9cd7542029d98
fx15164 <khoannfx15164@funix.edu.vn>
2022-08-20 15:56:05 +0700

    lab5.28

--- controllers/shop.js
+++ controllers/shop.js
@@ -111,16 +111,27 @@ exports.postCartDeleteProduct = (req, res, next) => {
 }
 
 exports.getOrders = (req, res, next) => {
-  res.render('shop/orders', {
-    path: '/orders',
-    pageTitle: 'Your Orders'
-  });
+  req.user
+  .getOrders({ include: ['products']})
+  .then(orders => {
+    console.log(orders);
+    res.render('shop/orders', {
+      path: '/orders',
+      pageTitle: 'Your Orders',
+      orders: orders
+    });
+  })
+  .catch(err => {
+    console.log(err);
+  })
 };
 
 exports.postOrder = (req, res, next) => {
+  let fetchedCart;
   req.user
     .getCart()
     .then(cart => {
+      fetchedCart = cart;
       return cart.getProducts();
     })
     .then(products => {
@@ -135,6 +146,9 @@ exports.postOrder = (req, res, next) => {
         .catch(err => {
           console.log(err);
         })
+    })
+    .then(result => {
+      return fetchedCart.setProducts(null);
     })
     .then(result => {
       res.redirect('/orders');
