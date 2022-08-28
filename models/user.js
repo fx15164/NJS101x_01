20dd89da38810501efc65dbdfd4071dffe623696
fx15164 <khoannfx15164@funix.edu.vn>
2022-08-28 13:45:23 +0700

    lab6.16

--- models/user.js
+++ models/user.js
@@ -4,14 +4,25 @@ const { getDb } = require('../util/database');
 const db = require('../util/database').getDb;
 
 class User  {
-    constructor(username, email) {
+    constructor(username, email, cart) {
         this.name = username;
         this.email = email;
+        this.cart = cart;
     }
 
     save() {
         const db = getDb();
         return db.collection('users').insertOne(this);
+    }
+
+    addToCard(product) {
+        const updatedCart = { items: [{...product, quantity: 1}] }
+        const db = getDb();
+        return db.collection('users')
+        .updateOne(
+            { _id: new ObjectId(this._id) },
+            { $set: { cart: updatedCart }}
+        )
     }
 
     static findById(userId) {
