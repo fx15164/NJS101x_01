fe069b06f21f64785a0cba074eaa96d7be163b55
fx15164 <khoannfx15164@funix.edu.vn>
2022-08-30 16:17:01 +0700

    lab8.4

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,20 +1,20 @@
 
 
 exports.getLogin = (req, res) => {
-    const isLoggedIn = 
-        req.get('Cookie')
-        .split(';')[0]
-        .trim()
-        .split('=')[1] === 'true';
+    // const isLoggedIn = 
+    //     req.get('Cookie')
+    //     .split(';')[0]
+    //     .trim()
+    //     .split('=')[1] === 'true';
 
     res.render('auth/login', {
         path: '/login',
         pageTitle: 'Login',
-        isAuthenticated: isLoggedIn
+        isAuthenticated: false
     })
 }
 
 exports.postLogin = (req, res) => {
-    res.setHeader('Set-Cookie', 'loggedIn=true');
+    res.setHeader('Set-Cookie', 'loggedIn=true;HttpOnly');
     res.redirect('/');
 }
