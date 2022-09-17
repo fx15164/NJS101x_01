62403a40da3349a3028b7d3b8cff50d23eead516
fx15164 <khoannfx15164@funix.edu.vn>
2022-09-17 22:40:41 +0700

    lab8.23

--- controllers/auth.js
+++ controllers/auth.js
@@ -4,10 +4,16 @@ const bcrypt = require('bcryptjs');
 
 
 exports.getLogin = (req, res) => {
+    let message = req.flash('error');
+    if (message.length > 0) {
+        message = message[0];
+    } else {
+        message = null;
+    }
     res.render('auth/login', {
         path: '/login',
         pageTitle: 'Login',
-        errorMessage: req.flash('error')
+        errorMessage: message
     })
 }
 
