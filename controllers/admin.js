bc84ef6ec00dd74ec453f5328dc02402492f6d74
fx15164 <khoannfx15164@funix.edu.vn>
2022-09-17 21:23:33 +0700

    lab8.17

--- controllers/admin.js
+++ controllers/admin.js
@@ -2,7 +2,7 @@ const Product = require('../models/product');
 const mongodb = require('mongodb');
 
 exports.getAddProduct = (req, res, next) => {
-  if (!req.isLoggedIn) {
+  if (!req.session.isLoggedIn) {
     return res.redirect('/login');
   }
   res.render('admin/edit-product', {
